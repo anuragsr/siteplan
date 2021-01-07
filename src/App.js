3499d09865c2125f61b7a1448046660fcc4be42c
Anurag Srivastava <anurag.131092@gmail.com>
2021-01-07 23:34:06 +0530

    Text change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now! Woah fast yeah!
+          react-three-fiber to be used here
         </a>
       </header>
     </div>
